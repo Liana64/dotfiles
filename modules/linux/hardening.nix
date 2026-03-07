ad591f1f869e600cf1159d31c5642aec402228f9
Liana <liana@lianas.org>
2026-03-07 11:58:54 -0600

    Add signal

--- modules/linux/hardening.nix
+++ modules/linux/hardening.nix
@@ -10,15 +10,18 @@
     };
   };
 
-  # Flatpaks are pretty good at sandboxing, so we ought to use them when available
-  services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-        # Only apps that are verified
-        # flatpak remote-add --if-not-exists --subset=verified flathub-verified https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
+  # flatpaks are pretty good at sandboxing, so we ought to use them when available
+  services.flatpak = {
+    enable = true;
+  };
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      # flatpak remote-add --if-not-exists --subset=verified flathub-verified https://flathub.org/repo/flathub.flatpakrepo
+      flatpak install -y --noninteractive flathub org.signal.Signal
+    '';
+  };
 }
