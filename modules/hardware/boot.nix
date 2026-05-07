2a8fc76974be9a57fdb15ed53e32ed55fa25b8a1
Liana <liana@lianas.org>
2026-05-07 07:36:54 -0500

    add kernel module blacklist

--- modules/hardware/boot.nix
+++ modules/hardware/boot.nix
@@ -42,5 +42,24 @@
 
     # TODO: Review kernel modules and disable unused
     #blacklistedKernelModules = [];
+    blacklistedKernelModules = [
+      # Obscure network protocols
+      "ax25" "netrom" "rose"
+
+      # Old or rare or insufficiently audited filesystems
+      "adfs" "affs"
+      "bfs" "befs"
+      "cramfs"
+      "efs" "erofs" "exofs"
+      "freevxfs" "f2fs"
+      "hfs" "hpfs"
+      "jfs"
+      "minix"
+      "nilfs2" "ntfs"
+      "omfs"
+      "qnx4" "qnx6"
+      "sysv"
+      "ufs"
+    ];
   };
 }
