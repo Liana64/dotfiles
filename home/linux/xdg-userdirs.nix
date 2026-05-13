b9231e0011006686dffc073e400069b2c9efd50b
Liana <liana@lianas.org>
2026-05-13 18:28:01 -0500

    change how screenshots are handled

--- home/linux/xdg-userdirs.nix
+++ home/linux/xdg-userdirs.nix
@@ -14,11 +14,12 @@
     templates   = null;
 
     extraConfig = {
-      XDG_NOTEBOOK_DIR  = "${config.home.homeDirectory}/Notebook";
-      XDG_REFERENCE_DIR = "${config.home.homeDirectory}/Reference";
-      XDG_PROJECTS_DIR  = "${config.home.homeDirectory}/Projects";
-      XDG_BACKUPS_DIR   = "${config.home.homeDirectory}/Backups";
-      XDG_MEDIA_DIR     = "${config.home.homeDirectory}/Media";
+      XDG_NOTEBOOK_DIR    = "${config.home.homeDirectory}/Notebook";
+      XDG_REFERENCE_DIR   = "${config.home.homeDirectory}/Reference";
+      XDG_PROJECTS_DIR    = "${config.home.homeDirectory}/Projects";
+      XDG_BACKUPS_DIR     = "${config.home.homeDirectory}/Backups";
+      XDG_MEDIA_DIR       = "${config.home.homeDirectory}/Media";
+      XDG_SCREENSHOTS_DIR = "${config.home.homeDirectory}/Media/Screenshots";
     };
   };
 }
