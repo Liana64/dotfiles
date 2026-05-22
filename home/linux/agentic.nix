c3832fd049f7b42e75f9c6589b05081fd318c8a7
Liana <liana@lianas.org>
2026-05-22 09:17:06 -0500

    configure claude code some more

--- home/linux/agentic.nix
+++ home/linux/agentic.nix
@@ -9,10 +9,28 @@ in
   programs.claude-code = {
     enable = true;
     package = nixpkgs-unstable.claude-code;
+    settings = {
+      teammateDefaultModel = "opus";
+      alwaysThinkingEnabled = true;
+      permissions.deny = [
+        "Read(./.env)"
+        "Read(./.env.*)"
+        "Read(./*.tfvars)"
+        "Read(./*kubeconfig)"
+        "Read(./secrets/**)"
+        "Read(./config/credentials.json)"
+        "Read(./build)"
+      ];
+      plugins = [
+        "rust-analyzer-lsp@claude-plugins-official"
+        "lua-lsp@claude-plugins-official"
+      ];
+    };
   };
 
   home.file = {
     ".claude/CLAUDE.md".source = ../../agentic/CLAUDE.md;
+    #".claude/.lsp.json".source = ../../agentic/lsp.json;
   } // lib.mapAttrs'
     (name: _: lib.nameValuePair ".claude/agents/${name}" {
       source = systemDir + "/${name}";
