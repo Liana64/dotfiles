77fcc6a3a696113bbe5df721a32cd99b3a9c2976
Liana <liana@lianas.org>
2026-05-23 14:21:31 -0500

    fix yubikey

--- home/linux/gpg.nix
+++ home/linux/gpg.nix
@@ -1,7 +1,12 @@
 {
   services.gpg-agent = {
-      enable = true;
-      defaultCacheTtl = 1800;
-      enableSshSupport = true;
-    };
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  programs.gpg = {
+    enable = true;
+    scdaemonSettings.disable-ccid = true;
+  };
 }
