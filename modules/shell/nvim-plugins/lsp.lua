2aaf7bdac4f16588e41ecb39e7ad70f2f8fe7c68
Liana <liana@lianas.org>
2026-08-03 17:43:01 -0500

    fix: lsp highlight

--- modules/shell/nvim-plugins/lsp.lua
+++ modules/shell/nvim-plugins/lsp.lua
@@ -1,6 +1,14 @@
+local builtin_highlight = vim.lsp.handlers["textDocument/documentHighlight"]
+
 ---@type vim.lsp.Config
 local defaults = {
   capabilities = require("blink.cmp").get_lsp_capabilities(),
+  handlers = {
+    ["textDocument/documentHighlight"] = function(err, result, ctx, cfg)
+      if not vim.api.nvim_buf_is_loaded(ctx.bufnr) then return end
+      return builtin_highlight(err, result, ctx, cfg)
+    end,
+  },
 }
 
 vim.lsp.config("*", defaults)
