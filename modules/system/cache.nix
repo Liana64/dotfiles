d0b1a47b09bc1818e1c0c7b4ccd0ccdbc12725be
Liana <liana@lianas.org>
2026-08-18 20:38:33 -0500

    fix: exempt pre-commit from cache cleanup

--- modules/system/cache.nix
+++ modules/system/cache.nix
@@ -1,8 +1,9 @@
 # @desc: Wipe XDG cache every 30 days
-{...}: {
+_: {
   flake.modules.homeManager.cache = {
     xdg.configFile."user-tmpfiles.d/cache.conf".text = ''
       R! %h/.cache/mozilla - - - -
+      x %h/.cache/pre-commit - - - -
       e %h/.cache - - - 30d
     '';
   };
