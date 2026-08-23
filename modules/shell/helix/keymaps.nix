25a97372b31f04f1bc0b70955d8ef92735563916
Liana <liana@lianas.org>
2026-08-23 09:00:34 -0500

    feat: more vim bindings for helix

--- modules/shell/helix/keymaps.nix
+++ modules/shell/helix/keymaps.nix
@@ -29,6 +29,19 @@
     s = "symbol_picker";
     t = "workspace_diagnostics_picker";
   };
+
+  motions = {
+    "0" = "goto_line_start";
+    "$" = "goto_line_end";
+    "^" = "goto_first_nonwhitespace";
+    "{" = "goto_prev_paragraph";
+    "}" = "goto_next_paragraph";
+    "%" = "match_brackets";
+    G = "goto_last_line";
+    H = "goto_window_top";
+    M = "goto_window_center";
+    L = "goto_window_bottom";
+  };
 in {
   flake.modules.homeManager.helix = {
     programs.helix.settings.keys = {
@@ -37,37 +50,42 @@ in {
         C-s = ["normal_mode" ":write"];
       };
 
-      select = {
-        C-s = ["normal_mode" ":write"];
-        "0" = "goto_line_start";
-        "$" = "goto_line_end";
-        "^" = "goto_first_nonwhitespace";
-        G = "goto_last_line";
-      };
+      select =
+        motions
+        // {
+          C-s = ["normal_mode" ":write"];
+          i = "select_textobject_inner";
+          a = "select_textobject_around";
+          o = "flip_selections";
+          p = "replace_with_yanked";
+          u = "switch_to_lowercase";
+          U = "switch_to_uppercase";
+          S = "surround_add";
+        };
 
       normal =
         tabs
+        // motions
         // {
           C-s = ":write";
-          C-r = "redo";
           C-v = "select_mode";
           C-n = "copy_selection_on_next_line";
           C-p = "copy_selection_on_prev_line";
-          "0" = "goto_line_start";
-          "$" = "goto_line_end";
-          "^" = "goto_first_nonwhitespace";
-          G = "goto_last_line";
-          C = ["kill_to_line_end" "insert_mode"];
+          C-e = "scroll_down";
+          C-y = "scroll_up";
+          C = ["extend_to_line_end" "change_selection"];
+          D = ["extend_to_line_end" "delete_selection"];
+          Y = ["extend_to_line_end" "yank"];
+          q = "record_macro";
+          "@" = "replay_macro";
           backspace = "move_char_left";
 
           "]".h = "goto_next_change";
           "[".h = "goto_prev_change";
 
-          C-w = {
-            h = "jump_view_left";
-            j = "jump_view_down";
-            k = "jump_view_up";
-            l = "jump_view_right";
+          Z = {
+            Z = ":write-quit";
+            Q = ":quit!";
           };
 
           space = {
@@ -100,6 +118,7 @@ in {
             b = "buffer_picker";
             C-e = "file_explorer";
             C-j = "file_explorer";
+            h = "select_all";
             "2" = ":hsplit";
             "3" = ":vsplit";
             "0" = "wclose";
