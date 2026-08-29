637f598c3905b9158d547f175b76d6ca5e9cd274
Liana <liana@lianas.org>
2026-08-29 09:15:29 -0500

    oob: update backup target

--- hosts/oob/backup.nix
+++ hosts/oob/backup.nix
@@ -5,7 +5,7 @@
     enable = true;
     interval = "daily";
     commands."rpool/var/unifi" = {
-      target = "backup@m1:tank/backups/oob";
+      target = "backup@m1.milberry.org:tank/backups/oob";
       sshKey = config.sops.secrets."backup/ssh-key".path;
       extraArgs = ["--no-sync-snap"];
     };
