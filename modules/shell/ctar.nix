6bdfb2448e3a218514c001f895b8a18eaf3eda40
Liana <liana@lianas.org>
2026-09-11 16:55:20 -0500

    feat: gpg tars

---
+++ modules/shell/ctar.nix
@@ -0,0 +1,23 @@
+# @desc: ctar — signed+encrypted tar archives using YubiKey
+{...}: {
+  flake.modules.homeManager.ctar = {
+    lib,
+    pkgs,
+    ...
+  }: let
+    deps = with pkgs; [coreutils findutils gnugrep gnupg gnutar gzip];
+    script =
+      pkgs.writeShellScriptBin "ctar"
+      (builtins.readFile ../bin/ctar);
+    ctar = pkgs.symlinkJoin {
+      name = "ctar";
+      paths = [script];
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/ctar --prefix PATH : ${lib.makeBinPath deps}
+      '';
+    };
+  in {
+    home.packages = [ctar];
+  };
+}
