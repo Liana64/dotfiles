c6c4a82eba1d7e38eb1571b318d5b4eff51d6c70
Liana <liana@lianas.org>
2026-09-11 14:24:05 -0500

    fix: framework nfs mounts

--- modules/system/nfs-client.nix
+++ modules/system/nfs-client.nix
@@ -6,19 +6,14 @@
       fsType = "nfs4";
       options = ["noauto" "soft" "x-systemd.automount" "x-systemd.idle-timeout=600"] ++ extra;
     };
-    clusterStorage = path: extra: {
-      device = "cluster.storage.milberry.org:/tank/${path}";
-      fsType = "nfs4";
-      options = ["noauto" "soft" "x-systemd.automount" "x-systemd.idle-timeout=600"] ++ extra;
-    };
   in {
     boot.supportedFilesystems = ["nfs"];
     fileSystems = {
       "/mnt/m1/liana" = homeStorage "home/liana" [];
       "/mnt/m1/shared" = homeStorage "home/shared" [];
       "/mnt/m1/landfill" = homeStorage "home/shared/landfill" [];
-      "/mnt/m1/photos" = clusterStorage "home/photos" ["ro"];
-      "/mnt/m1/media" = clusterStorage "media" [];
+      "/mnt/m1/photos" = homeStorage "home/photos" ["ro"];
+      "/mnt/m1/media" = homeStorage "media" [];
     };
 
     users.groups = {
